f8c7050ed7c0546d7c1cbc14d323f1c555e9911b
gokhnesen <gokhan_esen@hotmail.com.tr>
2022-08-15 11:45:22 +0300

    Test

--- DateApp.API/Startup.cs
+++ DateApp.API/Startup.cs
@@ -1,7 +1,9 @@
+using DateApp.Entity.DataContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,17 +18,20 @@ namespace DateApp.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContextModel>(options =>
+            {
+                options.UseSqlServer(_config.GetConnectionString("DateAppDb"));
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
---
+++ DateApp.Entity/DataContext/DataContextModel.cs
@@ -0,0 +1,19 @@
+﻿using DateApp.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateApp.Entity.DataContext
+{
+    public class DataContextModel:DbContext
+    {
+        public DataContextModel(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users  { get; set; }
+    }
+}
